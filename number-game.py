ea2e35f68b36cefb5e21e21c75d07e8436d4fbd6
SKB <skb@SKBs-MacBook-Pro.local>
2018-10-19 01:30:08 -0700

    First commit

---
+++ number-game.py
@@ -0,0 +1,39 @@
+import random
+
+
+def main():
+    gn = number_generator()
+    print(gn)
+    un = user_guess()
+    compare_value(un, gn)
+
+
+def number_generator():
+    generated_number = random.randint(1, 1000)
+    return generated_number
+
+
+def user_guess():
+    your_guess = int(input("Please guess a number: "))
+    return your_guess
+
+
+def compare_value(your_guess, generated_number):
+    i = 0
+    if your_guess != generated_number and i < 9:
+        i += 1
+        if your_guess > generated_number:
+            print("Guess Lower, {} tries left!".format(10-i))
+            user_guess()
+        else:
+            print("Guess Higher, {} tries left!".format(10-i))
+            user_guess()
+    else:
+        if your_guess == generated_number:
+            print("You Win!")
+        else:
+            print("You Lose!")
+
+
+if __name__ == '__main__':
+    main()
