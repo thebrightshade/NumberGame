e5ad43bf418af80afbc4e668a5678693d0959c25
SKB <skb@SKBs-MacBook-Pro.local>
2018-10-23 08:31:15 -0700

    Added try/except, which is not working as expected

--- number-game.py
+++ number-game.py
@@ -1,4 +1,7 @@
 import random
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -14,25 +17,38 @@ def number_generator():
 
 
 def user_guess():
-    your_guess = int(input("Please guess a number between 1 and 1000: "))
-    return your_guess
+    try:
+        your_guess = int(input("Please guess a number between 1 and 1000: "))
+        return your_guess
+    except Exception as error:
+        logger.error(error)
+        raise
 
 
 def compare_value(your_guess, generated_number):
     i = 1
     while your_guess != generated_number and i < 10:
         if your_guess > generated_number:
-            your_guess = int(
-                input("{} tries left! Guess Lower: ".format(10-i)))
+            try:
+                your_guess = int(
+                    input("{} tries left! Guess Lower: ".format(10-i)))
+            except Exception as error:
+                logger.error(error)
+                raise
         else:
-            your_guess = int(
-                input("{} tries left! Guess Higher: ".format(10-i)))
+            try:
+                your_guess = int(
+                    input("{} tries left! Guess Higher: ".format(10-i)))
+            except Exception as error:
+                logger.error(error)
+                raise
+
         i += 1
     else:
         if your_guess == generated_number:
             print("You Win! You guessed the number in {} tries.".format(i))
         else:
-            print("You Lose!")
+            print("You Lose! My number was {}".format(generated_number))
 
 
 if __name__ == '__main__':
